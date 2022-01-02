7d8fc2179e9e0446345301718e9efa179a756ce3
ridhalfajri00@gmail.com <Black.butterfly>
2022-01-03 01:21:45 +0700

    tambah tests.blade.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('frontpage.home');
 });
+Route::get('/welcome', function () {
+    return view('welcome');
+});
