6a0c2341d232efbb6e62bb54afa5d82f6d04ae2c
ridhalfajri00@gmail.com <Black.butterfly>
2022-01-02 23:54:00 +0700

    test coba

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h6>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae aliquid ea id non aliquam ab consequatur in sit quidem veritatis cupiditate maxime veniam voluptatem iusto, eius laboriosam molestias, enim iure.</h6>
+</body>
+</html>
