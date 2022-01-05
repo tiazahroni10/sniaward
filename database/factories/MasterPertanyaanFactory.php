091688a2f7b49763acbf216786585dcfbf6f3284
ridhalfajri00@gmail.com <Black.butterfly>
2022-01-05 14:02:21 +0700

    menambahkan factory

---
+++ database/factories/MasterPertanyaanFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MasterPertanyaanFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
