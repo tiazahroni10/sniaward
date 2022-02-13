fb8b28653d10a156711a0ff34f1b190be23e54da
ridhalfajri <ridhalfajri00@gmail.com>
2022-02-13 16:58:27 +0700

    fiksasi error berita

--- resources/views/frontpage/berita.blade.php
+++ resources/views/frontpage/berita.blade.php
@@ -22,91 +22,15 @@
                                     {{ $data->rilis }}
                                 </li>
                             </ul>
-<<<<<<< HEAD
                             <h3>{{ $data->judul }}</h3>
                             <p>{{ $data->potongan_berita }}</p>
-                            <a href="/{{ $data->slug }}" class="read-more">
-=======
-
-                            <h3>
-                                <a href="/detailberita">
-                                    Kumpulan Daftar Peraih SNI Awards 2021
-                                </a>
-                            </h3>
-                            <p>Acara SNI Award tahun 2021 diikuti oleh banyak peserta</p>
-                            <a href="/detailberita" class="read-more">
->>>>>>> Edit-Frontend
-                                Baca selengkapnya
-                            </a>
+                            <a href="/{{ $data->slug }}" class="read-more"> Baca selengkapnya</a>
                         </div>
                     </div>
                 </div>
                 @endforeach
 
-<<<<<<< HEAD
-=======
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog">
-                        <div class="image">
-                            <a href="/detailberita">
-                                <img src="{{ asset('assets') }}/img/blog/1.jpg" alt="image">
-                            </a>
-                            <div class="btn">
-                                <a href="/detailberita">Beberapa Kategori Pemenang SNI Award 2021</a>
-                            </div>
-                        </div>
-                        <div class="content">
-                            <ul class="post-meta">
-                                <li>
-                                    <i class="fa fa-calendar"></i>
-                                    18 Maret 2021
-                                </li>
-                            </ul>
 
-                            <h3>
-                                <a href="/detailberital">
-                                    Kategori Pemenang SNI Award Tahun 2021
-                                </a>
-                            </h3>
-                            <p>Acara SNI Award tahun 2021 diikuti oleh banyak peserta</p>
-                            <a href="/detailberita" class="read-more">
-                                Baca selengkapnya
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog">
-                        <div class="image">
-                            <a href="/detailberita">
-                                <img src="{{ asset('assets') }}/img/blog/1.jpg" alt="image">
-                            </a>
-                            <div class="btn">
-                                <a href="/detailberita">Penyerahan Hadiah SNI Award 2021</a>
-                            </div>
-                        </div>
-                        <div class="content">
-                            <ul class="post-meta">
-                                <li>
-                                    <i class="fa fa-calendar"></i>
-                                    28 Maret 2021
-                                </li>
-                            </ul>
-
-                            <h3>
-                                <a href="/detailberita">
-                                 Penyerahan Hadiah kepada Pemenang SNI Award 
-                                </a>
-                            </h3>
-                            <p>Acara SNI Award tahun 2021 diikuti oleh banyak peserta</p>
-                            <a href="/detailberita" class="read-more">
-                                Baca selengkapnya
-                            </a>
-                        </div>
-                    </div>
-                </div>
->>>>>>> Edit-Frontend
             </div>
         </div>
     </section>
