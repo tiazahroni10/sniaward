88c508a969179dd48f1bcd989ba29d10035451c6
tiazahroni10 <tiazayyzahroni@apps.ipb.ac.id>
2022-02-25 13:45:38 +0700

    fix session message halaman berita

--- resources/views/admin/berita/index.blade.php
+++ resources/views/admin/berita/index.blade.php
@@ -1,55 +1,58 @@
 @extends('layouts.admin.master')
 @section('content')
-
-
-<div class="content-body">
-    @if (session()->has('sukses'))
-				<div class="alert alert-success solid alert-dismissible fade show">
-					<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
-					<strong>{{ session('sukses') }}</strong>
-					<button type="button" class="close h-100" data-dismiss="alert" aria-label="Close"><span><i class="mdi mdi-close"></i></span>
-					</button>
-				</div>
-				@endif
+  <div class="content-body">
     <div class="container-fluid">
-        <div class="page-titles ">
-            <ol class="breadcrumb d-flex justify-content-between align-items-center">
-                <li class="breadcrumb-item active mr-auto"><a href="javascript:void(0)">Daftar Berita</a></li>
-                <li><div class="input-group search-area d-xl-inline-flex d-none mr-3">
-                    <div class="input-group-append">
-                        <span class="input-group-text"><a href="javascript:void(0)"><i class="flaticon-381-search-2"></i></a></span>
-                    </div>
-                    <input type="text" class="form-control" placeholder="Cari Berita . . .">
-                </div>
-                </li>
-                <li><a href="{{ route('berita.create') }}" class="btn btn-primary btn-event w-100" style="color: #ffffff">
-                    <span class="align-middle"><i class="ti-plus"></i></span> Tambah Berita
-                </a></li>
-                
-            </ol>
+      @if (session()->has('sukses'))
+        <div class="alert alert-success solid alert-dismissible fade show">
+          <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"
+            class="mr-2">
+            <polyline points="9 11 12 14 22 4"></polyline>
+            <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
+          </svg>
+          <strong>{{ session('sukses') }}</strong>
+          <button type="button" class="close h-100" data-dismiss="alert" aria-label="Close"><span><i class="mdi mdi-close"></i></span>
+          </button>
         </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-responsive-lg mb-0 table-striped" id="beritaTable">
-                                <thead>
-                                    <tr>
-                                        <th>Judul Berita</th>
-                                        <th>Tanggal Rilis</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+      @endif
+      <div class="page-titles ">
+        <ol class="breadcrumb d-flex justify-content-between align-items-center">
+          <li class="breadcrumb-item active mr-auto"><a href="javascript:void(0)">Daftar Berita</a></li>
+          <li>
+            <div class="input-group search-area d-xl-inline-flex d-none mr-3">
+              <div class="input-group-append">
+                <span class="input-group-text"><a href="javascript:void(0)"><i class="flaticon-381-search-2"></i></a></span>
+              </div>
+              <input type="text" class="form-control" placeholder="Cari Berita . . .">
             </div>
+          </li>
+          <li>
+            <a href="{{ route('berita.create') }}" class="btn btn-primary btn-event w-100" style="color: #ffffff">
+              <span class="align-middle"><i class="ti-plus"></i></span> Tambah Berita
+            </a>
+          </li>
+        </ol>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="card">
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-responsive-lg mb-0 table-striped" id="beritaTable">
+                  <thead>
+                    <tr>
+                      <th>Judul Berita</th>
+                      <th>Tanggal Rilis</th>
+                      <th>Aksi</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-
+  </div>
 @endsection
