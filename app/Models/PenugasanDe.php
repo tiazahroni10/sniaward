50fc9122df52727fb918644817bb81232f166462
ridhalfajri <ridhalfajri00@gmail.com>
2022-03-08 23:35:16 +0700

    migrate tabel

---
+++ app/Models/PenugasanDe.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PenugasanDe extends Model
+{
+    use HasFactory;
+    protected $table = "Penugasan_de";
+    protected $guarded = [''];
+}
