f32dd8db9fd165eb13e21d3f66e151873bb5ea1a
tia zahroni <tiazayyzahroni@apps.ipb.ac.id>
2022-03-23 10:26:51 +0700

    menambahkan halaman seputar sni award

--- resources/views/seputarsni.blade.php
+++ resources/views/seputarsni.blade.php
@@ -42,11 +42,8 @@
                 <div class="row">
                     <div class="col-lg-8 col-md-12">
                         <div class="blog-details-desc">
-                            <div class="article-image">
-                                <img src="" alt="image">
-                            </div>
                             <div class="article-content">
-                                <div class="entry-meta">
+                                <div class="entry-metanew">
                                     <ul>
                                         <li>
                                             <span>Diterbitkan :</span> 
@@ -55,12 +52,88 @@
                                     </ul>
                                 </div>
                                 <div class="mt-5">
-                                    
+                                    <p style="text-align: justify">SNI Award Merupakan tertinggi dari pemerintah Indonesia serta manjadi bagian dari upaya Pemerintah dalam menstimulasi peningkatan penerapan SNI oleh pelaku usaha maupun organisasi lainnya yang diselenggarakan oleh Badan Standardisasi Nasional (BSN). Melalui SNI Award diharapkan produsen, konsumen dan masyarakat umum semakin menghargai aspek mutu, dan memahami perlunya berpartisipasi aktif dalam pengembangan dan penggunaan SNI sebagai referensi penyediaan dan permintaan pasar.
+
+                                        Penilaian SNI Award pada hakekatnya berorientasi pada kegiatan standardisasi dan pemanfaatan SNI secara berkelanjutan. Kerangka kerja SNI Award mengacu pada Malcom Baldridge National Quality Award (MBNQA) yang telah dikembangkan dengan memperkuat muatan SNI sebagai ciri khas dari SNI Award. Penjabaran Kriteria penilaian yang dijadikan dasar untuk membuat pertanyaan mengacu pada SNI ISO 9004 dan MBNQA. Kegiatan evaluasi bukan merupakan audit atau pemastian mutu secara menyeluruh, namun merupakan kegiatan verifikasi atas dokumen yang disampaikan peserta dan memastikan penyesuaiannya di lapangan.
+                                    </p>
+                                    <b>SEJARAH</b>
+                                        
+                                    <p style="text-align: justify">SNI Award diselenggarakan sejak tahun 2005. dalam perkembangannya, SNI Award mengalami perubahan terkait kriteria penilaian, kategori peserta, sistem penilaian, susunan dewan juri maupun penghargaan . Di tahun 2020 SNI Award semakin mengukuhkan posisinya sebagai “The National Quality Award of Indonesia” dengan 12 kategori peserta yang melingkupi berbagai sektor jenis usaha maupun organisasi pendidikan. </p>
+                                        
+                                    <b>VISI</b>
+                                    <p style="text-align: justify">Menjadi landasan program peningkatan kinerja organisasi dalam rangka meningkatkan daya saing nasional. </p>
+                                        
+                                    <b>MISI</b>
+                                    <li>Meningkatkan penerapan SNI oleh organisasi secara lebih luas</li>
+                                    <li>Meningkatkan keberterimaan SNI dalam penilaian kinerja organisasi</li>
+                                    <li>Membangun role model organisasi penerap SNI yang berkinerja baik</li>
+                                     <br>   
+                                    <b>Manfaat Bagi Peserta</b>
+                                        <li>Peserta mendapat umpan balik hasil penilaian SNI Award yang dapat digunakan untuk meningkatkan kinerja organisasi</li>
+                                        <li>Penerima penghargaan akan dipromosikan melalui media publikasi BSN / massa </li>
+                                        <li>Dapat menggunakan logo SNI award dalam media promosi penerima anugerah </li>
+                                        <li>Penerima penghargaan yang produk/jasanya relevan akan mendapatkan rekomendasi dari BSN untuk mengikuti pameran yang diadakan oleh Kementerian terkait </li>
                                 </div>
                                 
                             </div>
                         </div>
-                    </div>    
+                    </div> 
+                    
+                    <div class="col-lg-4 col-md-12">
+                        <aside class="widget-area" id="secondary">
+                            <section class="widget widget_search">
+                                <form class="search-form search-top">
+                                    <label>
+                                        <span class="screen-reader-text">Search for:</span>
+                                        <input type="search" class="search-field" placeholder="Search...">
+                                    </label class="">
+                                    <button type="submit">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </form>
+                            </section>
+
+                            <section class="widget widget_colugo_posts_thumb">
+                                <h3 class="widget-title">Berita Terkini</h3>
+                                <article class="item">
+                                    <a href="#" class="thumb">
+                                        <span class="fullimage cover bg1" role="img"></span>
+                                    </a>
+                                    <div class="info">
+                                        <time class="2021-06-30">June 10, 2021</time>
+                                        <h4 class="title usmall">
+                                            <a href="index.html">Making Peace With The Feast Or Famine Of Freelancing</a>
+                                        </h4>
+                                    </div>
+                                </article>
+
+                                <article class="item">
+                                    <a href="#" class="thumb">
+                                        <span class="fullimage cover bg2" role="img"></span>
+                                    </a>
+                                    <div class="info">
+                                        <time class="2021-06-30">June 21, 2021</time>
+                                        <h4 class="title usmall">
+                                            <a href="index.html">Be healthy, Enjoy life with Colugo organic</a>
+                                        </h4>
+                                    </div>
+                                </article>
+
+                                <article class="item">
+                                    <a href="#" class="thumb">
+                                        <span class="fullimage cover bg3" role="img"></span>
+                                    </a>
+                                    <div class="info">
+                                        <time class="2021-06-30">June 30, 2021</time>
+                                        <h4 class="title usmall">
+                                            <a href="index.html">Buy organic food online and be healthy</a>
+                                        </h4>
+                                    </div>
+                                </article>
+                            </section>
+
+                        </aside>
+                    </div>
                 </div>
             </div>
         </section>
